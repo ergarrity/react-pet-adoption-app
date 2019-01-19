45c15aa02ff6ee8596dfa37c67e3a07dcef03500
Liz Garrity <ergarrity@gmail.com>
2019-01-19 15:01:10 -0800

    Added props input to Pet component and passed three different props objects to the instantiations within App component so that app now renders information for three distinct pets

--- src/app.js
+++ src/app.js
@@ -1,17 +1,29 @@
-const Pet = () => {
+const Pet = (props) => {
     return React.createElement("div", {}, [
-        React.createElement("h1", {}, "Callie"),
-        React.createElement("h2", {}, "Dog"),
-        React.createElement("h2", {}, "Australian Shepherd")
+        React.createElement("h1", {}, props.name),
+        React.createElement("h2", {}, props.animal),
+        React.createElement("h2", {}, props.breed)
     ]);
 }
 
 const App = () => {
     return React.createElement("div", {}, [
         React.createElement('h1', {}, 'Adopt Me!'),
-        React.createElement(Pet),
-        React.createElement(Pet),
-        React.createElement(Pet)        
+        React.createElement(Pet, {
+            name: "Callie",
+            animal: "dog",
+            breed: "Australian Shepherd"
+        }),
+        React.createElement(Pet, {
+            name: "Jerrold",
+            animal: "unicorn",
+            breed: "One of a kind"
+        }),
+        React.createElement(Pet, {
+            name: "Polly",
+            animal: "cat",
+            breed: "Princess of polymorphism"
+        })        
     ])
 }
 
