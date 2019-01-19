f2915fb76eb2ff0e7c718a9167628a0032d40497
Liz Garrity <ergarrity@gmail.com>
2019-01-19 14:51:59 -0800

    Created Pet component and instantiated three times within App component

--- src/app.js
+++ src/app.js
@@ -1,7 +1,18 @@
+const Pet = () => {
+    return React.createElement("div", {}, [
+        React.createElement("h1", {}, "Callie"),
+        React.createElement("h2", {}, "Dog"),
+        React.createElement("h2", {}, "Australian Shepherd")
+    ]);
+}
+
 const App = () => {
-    return React.createElement("div", {},
-        React.createElement('h1', {}, 'Adopt Me!')
-    )
+    return React.createElement("div", {}, [
+        React.createElement('h1', {}, 'Adopt Me!'),
+        React.createElement(Pet),
+        React.createElement(Pet),
+        React.createElement(Pet)        
+    ])
 }
 
 ReactDOM.render(React.createElement(App),  document.getElementById("root"))
