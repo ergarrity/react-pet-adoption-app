08db426984c74ac7373c9c9e7ca5fb898305cfd8
Liz Garrity <ergarrity@gmail.com>
2019-01-20 13:35:35 -0800

    Re-wrote App component in JSX

--- src/app.js
+++ src/app.js
@@ -7,28 +7,36 @@ class App extends React.Component {
     alert("you clicked the title");
   }
   render() {
-    return React.createElement("div", {}, [
-      React.createElement(
-        "h1",
-        { onClick: this.handleTitleClick },
-        "Adopt Me!"
-      ),
-      React.createElement(Pet, {
-        name: "Callie",
-        animal: "dog",
-        breed: "Australian Shepherd"
-      }),
-      React.createElement(Pet, {
-        name: "Jerrold",
-        animal: "unicorn",
-        breed: "One of a kind"
-      }),
-      React.createElement(Pet, {
-        name: "Polly",
-        animal: "cat",
-        breed: "Princess of polymorphism"
-      })
-    ]);
+    // return React.createElement("div", {}, [
+    //   React.createElement(
+    //     "h1",
+    //     { onClick: this.handleTitleClick },
+    //     "Adopt Me!"
+    //   ),
+    //   React.createElement(Pet, {
+    //     name: "Callie",
+    //     animal: "dog",
+    //     breed: "Australian Shepherd"
+    //   }),
+    //   React.createElement(Pet, {
+    //     name: "Jerrold",
+    //     animal: "unicorn",
+    //     breed: "One of a kind"
+    //   }),
+    //   React.createElement(Pet, {
+    //     name: "Polly",
+    //     animal: "cat",
+    //     breed: "Princess of polymorphism"
+    //   })
+    // ]);
+    return (
+      <div>
+        <h1>Adopt Me!</h1>
+        <Pet name="Callie" animal="dog" breed="Australian Shepherd" />
+        <Pet name="Jerold" animal="unicorn" breed="One of a kind" />
+        <Pet name="Polly" animal="cat" breed="Princess of polymorphism" />
+      </div>
+    )
   }
 }
 
