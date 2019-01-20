d857f98334fad9e4ac214c61bc3dd486113e78f2
Liz Garrity <ergarrity@gmail.com>
2019-01-20 13:51:06 -0800

    Configured eslint with react and removed click handler

--- src/app.js
+++ src/app.js
@@ -3,32 +3,7 @@ import { render } from 'react-dom';
 import Pet from './Pet';
 
 class App extends React.Component {
-  handleTitleClick() {
-    alert("you clicked the title");
-  }
   render() {
-    // return React.createElement("div", {}, [
-    //   React.createElement(
-    //     "h1",
-    //     { onClick: this.handleTitleClick },
-    //     "Adopt Me!"
-    //   ),
-    //   React.createElement(Pet, {
-    //     name: "Callie",
-    //     animal: "dog",
-    //     breed: "Australian Shepherd"
-    //   }),
-    //   React.createElement(Pet, {
-    //     name: "Jerrold",
-    //     animal: "unicorn",
-    //     breed: "One of a kind"
-    //   }),
-    //   React.createElement(Pet, {
-    //     name: "Polly",
-    //     animal: "cat",
-    //     breed: "Princess of polymorphism"
-    //   })
-    // ]);
     return (
       <div>
         <h1>Adopt Me!</h1>
