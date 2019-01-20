287eb0bd4ba785de26a32b4dbbfbbd183f4ea26c
Liz Garrity <ergarrity@gmail.com>
2019-01-20 10:38:20 -0800

    Added prettier

--- src/app.js
+++ src/app.js
@@ -1,37 +1,38 @@
-const Pet = (props) => {
-    return React.createElement("div", {}, [
-        React.createElement("h1", {}, props.name),
-        React.createElement("h2", {}, props.animal),
-        React.createElement("h2", {}, props.breed)
-    ]);
-}
+const Pet = props => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed)
+  ]);
+};
 class App extends React.Component {
-    handleTitleClick() {
-        alert("you clicked the title")
-    }
-    render() {
-        return React.createElement("div", {}, [
-            React.createElement('h1', 
-            { onClick: this.handleTitleClick},
-             'Adopt Me!'),
-            React.createElement(Pet, {
-                name: "Callie",
-                animal: "dog",
-                breed: "Australian Shepherd"
-            }),
-            React.createElement(Pet, {
-                name: "Jerrold",
-                animal: "unicorn",
-                breed: "One of a kind"
-            }),
-            React.createElement(Pet, {
-                name: "Polly",
-                animal: "cat",
-                breed: "Princess of polymorphism"
-            })        
-        ])
-
-    }
+  handleTitleClick() {
+    alert("you clicked the title");
+  }
+  render() {
+    return React.createElement("div", {}, [
+      React.createElement(
+        "h1",
+        { onClick: this.handleTitleClick },
+        "Adopt Me!"
+      ),
+      React.createElement(Pet, {
+        name: "Callie",
+        animal: "dog",
+        breed: "Australian Shepherd"
+      }),
+      React.createElement(Pet, {
+        name: "Jerrold",
+        animal: "unicorn",
+        breed: "One of a kind"
+      }),
+      React.createElement(Pet, {
+        name: "Polly",
+        animal: "cat",
+        breed: "Princess of polymorphism"
+      })
+    ]);
+  }
 }
 
-ReactDOM.render(React.createElement(App),  document.getElementById("root"))
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
