c7bc6b2343d3dcfdc03bd49a2ae3a6ad21594e13
Liz Garrity <ergarrity@gmail.com>
2019-01-20 14:22:25 -0800

    Imported petfinder client and addd componentDidMount() method to App component that logs the result of an API call to PetFinder for breed lists for dogs

--- src/app.js
+++ src/app.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import { render } from 'react-dom';
+import pf from 'petfinder-client';
 import Pet from './Pet';
 
+const petfinder = pf({
+  key: process.env.API_KEY,
+  secret: process.env.API_SECRET
+})
+
 class App extends React.Component {
+  componentDidMount() {
+    const promise = petfinder.breed.list({ animal: "dog" })
+    
+    promise.then(console.log, console.error);
+  }
   render() {
     return (
       <div>
@@ -11,7 +22,7 @@ class App extends React.Component {
         <Pet name="Jerold" animal="unicorn" breed="One of a kind" />
         <Pet name="Polly" animal="cat" breed="Princess of polymorphism" />
       </div>
-    )
+    );
   }
 }
 
